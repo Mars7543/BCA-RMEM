0767d475e9f1abd737b5d505d7260208c6ffd413
Mars7543 <miguelaroberts09@gmail.com>
2019-10-27 16:35:48 -0400

    Fixed Typo In Server.js

--- server.js
+++ server.js
@@ -17,15 +17,15 @@ mongoose
     .catch(err => console.log(`Error connecting to MongoDB:\n${err}`))
 
 // use routes
-// server.use('/api/users', require('./routes/api/users'))
-// server.use('/api/auth', require('./routes/api/auth'))
+server.use('/api/users', require('./routes/api/users'))
+server.use('/api/auth', require('./routes/api/auth'))
 server.use('/api/posts', require('./routes/api/posts'))
 
 // serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     server.use(express.static('client/build'))
 
-    app.get('*', (req, res) => {
+    server.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
